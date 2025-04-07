b701125ab95ecff553c8ba44073fffd38ba29ace
adrian <adrian.oliveira.dev@gmail.com>
2025-04-07 13:07:43 -0300

    scrapping data Brazil

---
+++ main.py
@@ -0,0 +1,30 @@
+from bs4 import BeautifulSoup
+import requests
+import csv
+
+html_text = requests.get('https://www.gov.br/anvisa/pt-br/setorregulado/regularizacao/agrotoxicos/monografias/monografias-autorizadas-por-letra')
+print('getting page ...')
+
+soup = BeautifulSoup(html_text.text, "lxml")
+
+ul_element = soup.find('ul', class_='toggable-content')
+
+# Open a CSV file to write the output
+with open('pesticidasPermitidos.csv', 'w', newline='', encoding='utf-8') as csvfile:
+    csv_writer = csv.writer(csvfile)
+    
+    csv_writer.writerow(['Nome do pesticida no Brasil', 'Detalhes Anvisa'])
+
+    ul_element = soup.find('ul', class_='toggable-content')
+
+    if ul_element:
+        anchor_elements = ul_element.find_all('a', href=lambda x: x and x.startswith("https://www.gov.br/anvisa/pt-br/setorregulado/regularizacao/agrotoxicos/monografias/monografias-autorizadas/"))
+        for anchor in anchor_elements:
+            if len(anchor.text) <= 6:
+                print(anchor.text)
+                continue
+            print(anchor.text)
+            print(len(anchor.text))
+            csv_writer.writerow([anchor.text[6:], anchor['href']]) 
+    else:
+        csv_writer.writerow(["No <ul> element with class 'toggable-content' found.", ""])  
