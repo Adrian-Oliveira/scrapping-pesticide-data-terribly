9255215fd80d3bb9a47e151f5dbd456a0c4ccf88
adrian <adrian.oliveira.dev@gmail.com>
2025-04-10 19:00:36 -0300

    getting braziliand name and english name

--- getPesticidesDetails.py
+++ getPesticidesDetails.py
@@ -6,23 +6,24 @@ import io
 
 def extract_data(text):
     data = {}
-    pattern = r'([^(]+)\s*$\s*([^)]+)\s*$'
-    
-    # Find all matches in the text
-    matches = re.findall(pattern, text)
+    pattern = r'comum:(.*?)(?=b\))'
+    # Search for the pattern in the text    
+    match = re.search(pattern, text, re.DOTALL) 
+    print('brazilian name and english name')
 
-    # Process and print the results
-    for match in matches:
-        before_parenthesis = match[0].strip()  # Value before the last parenthesis
-        inside_parenthesis = match[1].strip()   # Value inside the last parenthesis
-        print(f"Before Parenthesis: {before_parenthesis}")
-        print(f"Inside Parenthesis: {inside_parenthesis}")
+    if match:
+        result = match.group(1).strip()  # Get the captured group and strip whitespace
+        print("Extracted Text:")
+        print(result)
+    else:
+        print("No match found.")
 
 
     # Regular expressions to match the required fields
 
+    #'ingrediente_ativo': r'a\) Ingrediente ativo ou nome comum:\s*(.*)',
+
     patterns = {
-        'ingrediente_ativo': r'a\) Ingrediente ativo ou nome comum:\s*(.*)',
         'número_CAS': r'c\) N° CAS:\s*(.*)',
         'nome_químico': r'd\) Nome químico:\s*(.*)',
         'fórmula_bruta': r'e\) Fórmula bruta:\s*(.*)',
@@ -33,7 +34,6 @@ def extract_data(text):
     # Extract data using the defined patterns
     for key, pattern in patterns.items():
         match = re.search(pattern, text)
-        print(match)
         if match:
             data[key] = match.group(1).strip()  # Get the matched group and strip whitespace
     
@@ -41,8 +41,8 @@ def extract_data(text):
 
 
 
-pdf_url = "https://www.gov.br/anvisa/pt-br/setorregulado/regularizacao/agrotoxicos/monografias/monografias-autorizadas/a/4154json-file-1"
 pdf_url = "https://www.gov.br/anvisa/pt-br/setorregulado/regularizacao/agrotoxicos/monografias/monografias-autorizadas/a/acefato"
+pdf_url = "https://www.gov.br/anvisa/pt-br/setorregulado/regularizacao/agrotoxicos/monografias/monografias-autorizadas/a/4154json-file-1"
 # Send a GET request to download the PDF
 print('buscando dados')
 response = requests.get(pdf_url)
