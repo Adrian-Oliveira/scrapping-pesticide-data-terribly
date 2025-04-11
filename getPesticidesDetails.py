f5ad501db7ecbd66af3cd045fc32a000fdaa1b4d
adrian <adrian.oliveira.dev@gmail.com>
2025-04-11 20:27:02 -0300

    getting pesticide Pt-br name and En name

--- getPesticidesDetails.py
+++ getPesticidesDetails.py
@@ -4,17 +4,42 @@ from pdfminer.high_level import extract_pages, extract_text
 import tabula
 import io
 
+
+def extract_name_Ptbr_and_En(text:str)-> list[str]:
+    l, r = 0, len(text) - 1
+    print(text)
+
+    while text[r] != ')':
+        r -= 1
+
+    l = r 
+    countOpenParentheses = 1
+    while countOpenParentheses:
+        l -= 1
+        if text[l] == ')':
+            countOpenParentheses += 1
+        if text[l] == '(':
+            countOpenParentheses -= 1
+
+    
+    print('Portuguese name')
+    print(text[:l].strip())
+    print('English name')
+    print(text[l+1:r].strip())
+    
+    return [text[:l], text[l+1:r]]
+ 
 def extract_data(text):
     data = {}
     pattern = r'comum:(.*?)(?=b\))'
     # Search for the pattern in the text    
     match = re.search(pattern, text, re.DOTALL) 
-    print('brazilian name and english name')
 
     if match:
         result = match.group(1).strip()  # Get the captured group and strip whitespace
         print("Extracted Text:")
-        print(result)
+        extract_name_Ptbr_and_En(result)
+
     else:
         print("No match found.")
 
@@ -41,8 +66,8 @@ def extract_data(text):
 
 
 
-pdf_url = "https://www.gov.br/anvisa/pt-br/setorregulado/regularizacao/agrotoxicos/monografias/monografias-autorizadas/a/acefato"
 pdf_url = "https://www.gov.br/anvisa/pt-br/setorregulado/regularizacao/agrotoxicos/monografias/monografias-autorizadas/a/4154json-file-1"
+pdf_url = "https://www.gov.br/anvisa/pt-br/setorregulado/regularizacao/agrotoxicos/monografias/monografias-autorizadas/a/acefato"
 # Send a GET request to download the PDF
 print('buscando dados')
 response = requests.get(pdf_url)
@@ -57,10 +82,11 @@ if response.status_code == 200:
 
     extracted_data = extract_data(text)
 
+    """     
     for key, value in extracted_data.items():
         print(value) if value[-1] == ')' else None
         print(f"{key.replace('_', ' ').title()}: {value}")
-
+    """
 else:
     print(f"Failed to download PDF. Status code: {response.status_code}")
 
